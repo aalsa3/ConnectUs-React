d39e984ad9b5fce84a5ac69dc27551e132ff8433
unknown <aliyarahmanovic>
2019-10-15 13:54:07 +1000

    reminders kind of done

--- navigation/DrawerScreen.js
+++ navigation/DrawerScreen.js
@@ -3,23 +3,17 @@ import {View, Text, Button, TouchableOpacity, InteractionManager, AsyncStorage, 
 import {withNavigation, NavigationActions, StackActions} from 'react-navigation';
 import * as Firebase from '../components/Firebase';
 
+import HowScreen from '../screens/HowScreen';
+import { createDrawerNavigator } from 'react-navigation-drawer';
+import { createAppContainer } from 'react-navigation';
+
 import MotionSlider from 'react-native-motion-slider'
 
 class DrawerScreen extends React.Component {
   constructor(props) {
     super(props);
   }
-      // logout = async () => {
-      //                         try {
-      //                           console.log('fromDrawer')
-      //                           Firebase.logoutUser();
-      //                           AsyncStorage.clear();
-      //                           this.props.navigation.navigate("Auth");
-      //                         } catch (e) {
-      //                           console.log(e);
-      //                         }
-      //                       }
-                
+
     logout(navigation) {
       console.log('logout called', navigation)
       Firebase.logoutUser();
@@ -35,6 +29,12 @@ class DrawerScreen extends React.Component {
                 <Text>Side Bar Info</Text>
 
                 <Button
+                    title='How are you?' 
+                    onPress={() => {
+                    this.props.navigation.navigate('Howareyou');
+                    }}/>
+
+                <Button
                     title = 'Logout'
                     onPress ={() => {
                         this.logout(this.props.navigation)
@@ -46,7 +46,33 @@ class DrawerScreen extends React.Component {
     }
 }
 
-export default withNavigation(DrawerScreen);
+class HowAreYou extends React.Component {
+  static navigationOptions = {
+    title: 'Howareyou'
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text
+          style={styles.paragraph}
+          onPress={() => {
+            this.props.navigation.navigate('Howareyou');
+          }}>
+          Go back home
+        </Text>
+      </View>
+    );
+  }
+}
+
+// const navigator = createDrawerNavigator(
+//   {
+//     HowAreYou: HowScreen
+//   });
+
+  //export default createAppContainer(navigator);
+  export default withNavigation(DrawerScreen);
 
 
 const styles = StyleSheet.create({
